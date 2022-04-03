d68350c769a81f98636db53850bed2acc110bb0b
MANDAPAVANKUMAR06071999 <pavankumarmanda6@gmail.com>
2022-04-03 23:14:59 +0530

    rrrr

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ app.get('/welcome',welcomeController.welcomefun);
 
 
 app.listen(6002,()=>{
-    console.log("Server started and running at port number 6002");
+    console.log("Server started and runing at port number 6002");
 })
 
